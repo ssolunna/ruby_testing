edcbee275275df6e01428795fb57312a689ddfb9
ssolunna <ssolunna29@gmail.com>
2023-08-23 10:52:24 -0500

    Complete the Assignment no. 11 (for the Dog class)

--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,21 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
+  subject(:hercules) { described_class.new('Hercules', nil, 'black') }
 
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
   context '' do
+    it_behaves_like 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(hercules).not_to be_barking
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(hercules).to be_sleeping
   end
 end
